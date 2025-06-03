7065bd35b0bd15e9584eb9ce502be2bde6985001
De General <aubynsamuel05@gmail.com>
2025-06-03 06:40:00 +0000

    compile sdk updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.aubynsamuel.clipsync"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.aubynsamuel.clipsync"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
